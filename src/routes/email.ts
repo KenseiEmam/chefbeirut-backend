5440843f54e8a0f306a35196b59711b6bfa6f500
KenseiEmam <mohamed.ashraf2k@gmail.com>
2025-09-27 14:01:31 +0400

    add emailer api fix

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -15,6 +15,7 @@ router.post("/send", async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
+      secure: true, 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
