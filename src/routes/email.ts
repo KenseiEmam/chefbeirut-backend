2985597435c029fe650e1f01442079ac99cdb6fd
KenseiEmam <mohamed.ashraf2k@gmail.com>
2025-09-27 13:52:49 +0400

    add emailer api fix

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -15,7 +15,6 @@ router.post("/send", async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
-      secure: false, // Elastic uses TLS but usually not "secure" true unless 465
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
